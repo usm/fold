258410aed9dcec3f4fcdb8574c8e8eb7ade00e32
Jonas Almeida <jalmeida@mathbiol.org>
2024-01-25 10:17:52 -0500

    forward and backward ACGT plots together

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ console.log(`index.js loaded\n${Date()}`);
     
     // load USM v3 module
     Umod = (await import(`https://usm.github.io/3/usm.mjs`))
-    // Umod = (await import(`http://localhost:8000/usm3/usm.mjs`))
+    //Umod = (await import(`http://localhost:8000/usm3/usm.mjs`))
     
     function plotUSM(){
         taCGR.value=taCGR.value.toUpperCase().replace(/[^ACGT]/g,'')
-        let direction = [direction_forward,direction_backward].filter(ip=>ip.checked==true)[0].value
+        //let direction = [direction_forward,direction_backward].filter(ip=>ip.checked==true)[0].value
         let seed = [seed_middle,seed_circular,seed_bidirectional].filter(ip=>ip.checked==true)[0].value
-        let size = plotSize.value
+        let size = parseInt(plotSize.value)+300
         let u = new Umod.USM(taCGR.value,seed,['A','C','G','T'])
-        u.plotACGT(divPlotUSM,size,direction)
+        //u.plotACGT(divPlotUSM,size,direction)
         //console.log(u)
+        u.plotACGT(forwardACGT,size,'forward')
+        u.plotACGT(backwardACGT,size,'backward')
     }
 
     function plotCanvas(){
